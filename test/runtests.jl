0570f7e64ce5246da3303fd964d2d7f4b820456a
Bill <wherrera10@users.noreply.github.com>
2021-06-22 07:33:10 -1000

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using RxNav
+using Test
+
+@test rcui("ibuprofen") = 0
+@test contains(drugs("naproxen"), "")
+@test contains(interaction("61148", ONCHigh = false)[1], "")
+@test contains(interaction("sumatriptan")[1], "")
+@test contains(interaction_within_list(["207106", "656659"]))
+@test contains(interaction_within_list(["divalproex", "lamotrigine"]))
+
+    
