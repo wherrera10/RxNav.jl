4b25e0ba1dd1e43bdf0833ecbe9a98023df827fe
Bill <wherrera10@users.noreply.github.com>
2021-06-26 12:15:19 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using RxNav
 using Test
 
 @test rcui("ibuprofen") == "5640"
-@test contains(drugs("naproxen"), "")
-@test contains(interaction("61148", ONCHigh = false)[1], "")
-@test contains(interaction("sumatriptan")[1], "")
-@test contains(interaction_within_list(["207106", "656659"]))
-@test contains(interaction_within_list(["divalproex", "lamotrigine"]))
+@test contains(first(drugs("naproxen")), "")
+@test contains(first(interaction("61148", ONCHigh = false)), "")
+@test contains(first(interaction("sumatriptan")), "")
+@test contains(interaction_within_list(["207106", "656659"]), "")
+@test contains(interaction_within_list(["divalproex", "lamotrigine"]), "")
 
     
