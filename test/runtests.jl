82f9fd2d52ba7b92f0d1e9b65203d0b161570e80
Bill <wherrera10@users.noreply.github.com>
2021-06-26 13:38:32 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,13 @@ using RxNav
 using Test
 
 @test rcui("ibuprofen") == "5640"
-@test contains(first(drugs("naproxen")), "")
-@test contains(first(interaction("61148", ONCHigh = false)), "")
-@test contains(first(interaction("sumatriptan")), "")
-@test contains(interaction_within_list(["207106", "656659"]), "")
-@test contains(interaction_within_list(["divalproex", "lamotrigine"]), "")
 
-    
+@test contains(first(drugs("naproxen")), "Oral")
+
+@test contains(last(interaction("61148"; ONCHigh = false)).description, "creased")
+
+@test first(interaction("sumatriptan")).severity == "high"
+
+@test contains(last(interaction_within_list(["207106", "656659"])).description, "metabolism")
+
+@test last(interaction_within_list(["divalproex", "lamotrigine"])).severity == "N/A" 
