0ed43c96d0e7ee3f7e2cd4cf693a6513bc324ea2
Bill <wherrera10@users.noreply.github.com>
2026-09-18 16:06:12 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,8 @@ using Test
 
 @test contains(first(drugs("naproxen")), "Oral")
 
+println("going on to spelling")
+
 #@test contains(last(interaction("61148"; ONCHigh = false)).description, "creased")
 
 #@test first(interaction("sumatriptan")).severity == "high"
@@ -20,6 +22,4 @@ RxNav.getSpellingSuggestions("nortriptelene") == ["nortriptyline"]
 prescribable(false)
 RxNav.getSpellingSuggestions("nortriptelene") == ["nortriptyline", "Nortriptylina"]
 
-println("Done")
 true
-
